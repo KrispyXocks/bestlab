b9bedc0930d41ea3f63d7746d8763e879cdd0e80
KrispyXocks <95459496+KrispyXocks@users.noreply.github.com>
2024-08-22 08:13:17 +0700

    date & time final

--- bestlab/lib/pages/webview_test.dart
+++ bestlab/lib/pages/webview_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class DateTimeRangePickerExample extends StatefulWidget {
@@ -12,90 +13,210 @@ class DateTimeRangePickerExample extends StatefulWidget {
 class _DateTimeRangePickerExampleState extends State<DateTimeRangePickerExample> {
   String _selectedFromDateTime = "Select From Date & Time";
   String _selectedToDateTime = "Select To Date & Time";
-  String _baseUrl = "http://10.0.2.2:3000/d-solo/TXSTREZ/simple-streaming-example?orgId=1&panelId=5";
+  String _baseUrl = "http://localhost:3000/d-solo/TXSTREZ/simple-streaming-example?orgId=1&panelId=5";
   late WebViewController _webViewController;
 
   final String _defaultFromDateTime = "2024-01-01T00:00:00Z"; // Giá trị mặc định cho "from"
   final String _defaultToDateTime = "now"; // Giá trị mặc định cho "to"
 
+  DateTime? selectedDate = DateTime.now();
+  TimeOfDay? selectedTime = TimeOfDay.now();
+
   Future<void> _selectFromDateTime(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
+    await showDialog(
       context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
+      builder: (BuildContext context) {
+        return Dialog(
+          child: DefaultTabController(
+            length: 2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TabBar(
+                  tabs: [
+                    Tab(text: "Date"),
+                    Tab(text: "Time"),
+                  ],
+                ),
+                Container(
+                  height: 300, // Chiều cao của TabBarView
+                  child: TabBarView(
+                    children: [
+                      // Tab Date Picker
+                      Center(
+                        child: SizedBox(
+                          height: 250,
+                          child: CalendarDatePicker(
+                            initialDate: selectedDate ?? DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2101),
+                            onDateChanged: (date) {
+                              selectedDate = date;
+                            },
+                          ),
+                        ),
+                      ),
+                      // Tab Time Picker Custom
+                      Center(
+                        child: SizedBox(
+                          height: 250,
+                          child: _buildCustomTimePicker(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (selectedDate != null && selectedTime != null) {
+                          final DateTime fullDateTime = DateTime(
+                            selectedDate!.year,
+                            selectedDate!.month,
+                            selectedDate!.day,
+                            selectedTime!.hour,
+                            selectedTime!.minute,
+                          );
+                          String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(fullDateTime);
+                          formattedDate = "${formattedDate}Z";
 
-    if (pickedDate != null) {
-      final TimeOfDay? pickedTime = await showTimePicker(
-        context: context,
-        initialTime: TimeOfDay.fromDateTime(DateTime.now()),
-        builder: (context, child) {
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-            child: child!,
-          );
-        },
-        initialEntryMode: TimePickerEntryMode.input, // Đặt chế độ nhập text
-      );
-
-      if (pickedTime != null) {
-        final DateTime fullDateTime = DateTime(
-          pickedDate.year,
-          pickedDate.month,
-          pickedDate.day,
-          pickedTime.hour,
-          pickedTime.minute,
+                          setState(() {
+                            _selectedFromDateTime = formattedDate;
+                          });
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("OK"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         );
-
-        String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(fullDateTime);
-        formattedDate = "${formattedDate}Z";
-
-        setState(() {
-          _selectedFromDateTime = formattedDate;
-        });
-      }
-    }
+      },
+    );
   }
 
   Future<void> _selectToDateTime(BuildContext context) async {
-    final DateTime? pickedDate = await showDatePicker(
+    await showDialog(
       context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2101),
-    );
+      builder: (BuildContext context) {
+        return Dialog(
+          child: DefaultTabController(
+            length: 2,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TabBar(
+                  tabs: [
+                    Tab(text: "Date"),
+                    Tab(text: "Time"),
+                  ],
+                ),
+                Container(
+                  height: 300, // Chiều cao của TabBarView
+                  child: TabBarView(
+                    children: [
+                      // Tab Date Picker
+                      Center(
+                        child: SizedBox(
+                          height: 250,
+                          child: CalendarDatePicker(
+                            initialDate: selectedDate ?? DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2101),
+                            onDateChanged: (date) {
+                              selectedDate = date;
+                            },
+                          ),
+                        ),
+                      ),
+                      // Tab Time Picker Custom
+                      Center(
+                        child: SizedBox(
+                          height: 250,
+                          child: _buildCustomTimePicker(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        if (selectedDate != null && selectedTime != null) {
+                          final DateTime fullDateTime = DateTime(
+                            selectedDate!.year,
+                            selectedDate!.month,
+                            selectedDate!.day,
+                            selectedTime!.hour,
+                            selectedTime!.minute,
+                          );
+                          String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(fullDateTime);
+                          formattedDate = "${formattedDate}Z";
 
-    if (pickedDate != null) {
-      final TimeOfDay? pickedTime = await showTimePicker(
-        context: context,
-        initialTime: TimeOfDay.fromDateTime(DateTime.now()),
-        builder: (context, child) {
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-            child: child!,
-          );
-        },
-        initialEntryMode: TimePickerEntryMode.input, // Đặt chế độ nhập text
-      );
-
-      if (pickedTime != null) {
-        final DateTime fullDateTime = DateTime(
-          pickedDate.year,
-          pickedDate.month,
-          pickedDate.day,
-          pickedTime.hour,
-          pickedTime.minute,
+                          setState(() {
+                            _selectedToDateTime = formattedDate;
+                          });
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("OK"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         );
+      },
+    );
+  }
 
-        String formattedDate = DateFormat("yyyy-MM-ddTHH:mm:ss").format(fullDateTime);
-        formattedDate = "${formattedDate}Z";
-
-        setState(() {
-          _selectedToDateTime = formattedDate;
-        });
-      }
-    }
+  Widget _buildCustomTimePicker() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        NumberPicker(
+          value: selectedTime!.hour,
+          minValue: 0,
+          maxValue: 23,
+          onChanged: (value) {
+            setState(() {
+              selectedTime = TimeOfDay(hour: value, minute: selectedTime!.minute);
+            });
+          },
+        ),
+        Text(":"),
+        NumberPicker(
+          value: selectedTime!.minute,
+          minValue: 0,
+          maxValue: 59,
+          onChanged: (value) {
+            setState(() {
+              selectedTime = TimeOfDay(hour: selectedTime!.hour, minute: value);
+            });
+          },
+        ),
+      ],
+    );
   }
 
   void _selectNow() {
